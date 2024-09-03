095dfe0eac3112a5698cd87d5d545838c21af976
zxuan32 <zxuan.av1@gmail.com>
2024-09-03 00:36:35 -0600

    feat: Se agrego la imagen para fondo
    - Se agergo una imagen 'nier.webp' al body

--- practica_6/lib/automatas.dart
+++ practica_6/lib/automatas.dart
@@ -7,8 +7,19 @@ class Automatas extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Otra pagina 💀🥵'),
+        title: const Text('Nier 🥵'),
         backgroundColor: Colors.black38,
+      ),
+      body: Stack(
+        children: [
+          // Use Image.asset to display the image from assets
+          Image.asset(
+            'nier.webp', // Make sure the image path is correct and added in pubspec.yaml
+            fit: BoxFit.cover, // Adjust the image fit
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ],
       ),
     );
   }
